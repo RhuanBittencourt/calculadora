fc1dc74d85d9da11398b999a71757b0052262f78
Rhuann Bittencourt <rhuanbittencourtcordeiro22@gmail.com>
2024-09-27 16:31:05 -0300

    Fazendo alteração no css e no js

--- script.js
+++ script.js
@@ -15,15 +15,15 @@ function calcular(event) {
     
 
     if(imc < 17) {
-        resultado.innerHTML = '<br> Seu resultado foi ' + imc.toFixed(2) + ' | você está muito abaixo do peso!'
+        resultado.innerHTML = '<br> Seu resultado foi ' + imc.toFixed(2) + ' | <span>você está muito abaixo do peso!</span>'
     }else if( imc >= 17 && imc < 18.49) {
-        resultado.innerHTML = '<br> Seu resultado foi ' + imc.toFixed(2) + ' | você está abaixo do peso!'
+        resultado.innerHTML = '<br> Seu resultado foi ' + imc.toFixed(2) + ' | <span>você está abaixo do peso!</span>'
     }else if(imc >= 18.49 && imc < 24.99) {
-        resultado.innerHTML = '<br> Seu resultado foi ' + imc.toFixed(2) + ' | você está no peso ideal!'
+        resultado.innerHTML = '<br> Seu resultado foi ' + imc.toFixed(2) + '<br>  <span id="ideal">você está no peso ideal!</span>'
     }else if(imc >= 25 && imc < 29.99) {
-        resultado.innerHTML = '<br> Seu resultado foi ' + imc.toFixed(2) + ' | você está acima do peso!'
+        resultado.innerHTML = '<br> Seu resultado foi ' + imc.toFixed(2) + ' |<span> você está acima do peso!</span>'
     }else if(imc > 30) {
-        resultado.innerHTML = '<br> Seu resultado foi ' + imc.toFixed(2) + ' | Cuidado, obesidade!!'
+        resultado.innerHTML = '<br> Seu resultado foi ' + imc.toFixed(2) + ' | <span>Cuidado, obesidade!!</span>'
     }
 
     document.getElementById('peso').value = '';
